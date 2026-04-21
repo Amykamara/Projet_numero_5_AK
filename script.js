012a757a0b8c8839652bff8dae55239549319eab
Aminata <a.kamaraminata@gmail.com>
2026-04-21 20:41:59 +0200

    ajout txt

--- script.js
+++ script.js
@@ -2,43 +2,70 @@
 // Variables 
 const btnLeft = document.querySelector(".arrow_left");
 const btnright =document.querySelector(".arrow_right");
-let slide = ["./assets/images/slideshow/slide1.jpg","./assets/images/slideshow/slide2.jpg", "./assets/images/slideshow/slide3.jpg","./assets/images/slideshow/slide4.png"];
+const slides = [
+	{
+		image: "slide1.jpg",
+		tagLine: "Impressions tous formats <span>en boutique et en ligne</span>"
+	},
+	{
+		image: "slide2.jpg",
+		tagLine: "Tirages haute définition grand format <span>pour vos bureaux et events</span>"
+	},
+	{
+		image: "slide3.jpg",
+		tagLine: "Grand choix de couleurs <span>de CMJN aux pantones</span>"
+	},
+	{
+		image: "slide4.png",
+		tagLine: "Autocollants <span>avec découpe laser sur mesure</span>"
+	}
+];
+// let slide = ["./assets/images/slideshow/slide1.jpg","./assets/images/slideshow/slide2.jpg", "./assets/images/slideshow/slide3.jpg","./assets/images/slideshow/slide4.png"];
 const banner = document.querySelector(".banner-img");
+const tagLine = document.querySelector(".tagline");
 let index = 0;
 let dots = document.querySelectorAll(".dot");
 console.log(dots);
 
 // Je déclare/
-banner.src= slide[index];
+banner.src = `./assets/images/slideshow/${slides[index].image}`;
+tagLine.innerHTML = slides[index].tagLine;
 dots[index].classList.add("dot_selected");
 
 
 // au clique Gauche
 
-btnLeft.addEventListener("click" , function() {
+btnLeft.addEventListener("click", function() {
     dots[index].classList.remove("dot_selected");
-    
-index --;
-    if (index < 0 ) {
-    index = slide.length -1  ;
-    
-}
-banner.src= slide[index];
-dots[index].classList.add("dot_selected");
+
+    index--;
+
+    if (index < 0) {
+        index = slides.length - 1;
+    }
+
+    banner.src = `./assets/images/slideshow/${slides[index].image}`;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    dots[index].classList.add("dot_selected");
 });
 
 
 // click droit 
 
-btnright.addEventListener("click" , function() {
+btnright.addEventListener("click", function() {
     dots[index].classList.remove("dot_selected");
-index++;
-    if (index > slide.length -1) {
-    index = 0;
-    
-}
-banner.src= slide[index];
-dots[index].classList.add("dot_selected");
+
+    index++;
+
+    if (index > slides.length - 1) {
+        index = 0;
+    }
+
+    banner.src = `./assets/images/slideshow/${slides[index].image}`;
+    tagLine.innerHTML = slides[index].tagLine;
+
+    dots[index].classList.add("dot_selected");
 });
 
 
